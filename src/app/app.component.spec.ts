69a52aa0b0bb7e0265b303068c371b0c77624ad6
Jhon Guzman <jfguzman@indracompany.com>
2022-10-29 21:35:50 -0500

    se envia actividad realizada

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book/book-list/book-list.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
-    }).compileComponents();
-  });
+ beforeEach(async () => {
+   await TestBed.configureTestingModule({
+     imports: [RouterTestingModule, HttpClientModule],
+     declarations: [AppComponent, BookListComponent],
+   }).compileComponents();
+ });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+ it('should create the app', () => {
+   const fixture = TestBed.createComponent(AppComponent);
+   const app = fixture.componentInstance;
+   expect(app).toBeTruthy();
+ });
 
-  it(`should have as title 'base-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('base-project');
-  });
+ it(`should have as title 'base-project'`, () => {
+   const fixture = TestBed.createComponent(AppComponent);
+   const app = fixture.componentInstance;
+   expect(app.title).toEqual('base-project');
+ });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'base-project app is running!'
-    );
-  });
 });
+/* Archivo src/app/app.component.spec.ts */
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const baseUrl = 'https://misw4104-api.herokuapp.com/api/';
+
 export const environment = {
-  production: false
+ production: false,
+ baseUrl
 };
 
 /*
